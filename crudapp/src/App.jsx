6ab6590db0641c79d19311bd758992e19bd8f2e0
Shubham Kumar <101807753+ShubhamGoutam789571@users.noreply.github.com>
2022-11-14 22:13:59 +0530

    Crud_App
    crud app in Mern

---
+++ crudapp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+     <>
+      <h3>HEllo Shubham App</h3>
+     </>
+  )
+}
+
+export default App
